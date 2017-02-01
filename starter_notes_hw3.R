742cb64955d21e9efa6af5ee64141d84438c1a2d
garnerat <atg530@gmail.com>
2017-01-31 20:51:53 -0500

    initial commit

---
+++ starter_notes_hw3.R
@@ -0,0 +1,15 @@
+# Starter code for German credit scoring
+
+# Refer to http://archive.ics.uci.edu/ml/datasets/Statlog+(German+Credit+Data) for variable description. 
+#Notice that “It is worse to class a customer as good when they are bad (5), than it is to class a customer as bad when they are good (1).” 
+#Define your cost function accordingly!
+  
+  german_credit = read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data")
+
+colnames(german_credit) = c("chk_acct", "duration", "credit_his", "purpose", 
+                            "amount", "saving_acct", "present_emp", "installment_rate", "sex", "other_debtor", 
+                            "present_resid", "property", "age", "other_install", "housing", "n_credits", 
+                            "job", "n_people", "telephone", "foreign", "response")
+
+# orginal response coding 1= good, 2 = bad we need 0 = good, 1 = bad
+german_credit$response = german_credit$response - 1
