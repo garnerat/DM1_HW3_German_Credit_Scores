48bc6e53cf0c50ca304f01d5c769397f27ba3723
garnerat <atg530@gmail.com>
2017-02-05 11:34:24 -0500

    initial commit

---
+++ HW3_1_logit_probit_simul.R
@@ -0,0 +1,11 @@
+#HW3 Problem 1
+
+##### Simulation Study: Logistic/Probit Regression #####
+
+# y|x ~ Binary(p), p = E(y|x),
+# logit(pi) = -1.1 + 5*x1i -0.4*x2i
+# x1i ~ Unif(0,1) 
+# x2i ~ 1,0 for odd/even i's respectively
+
+n=500
+
